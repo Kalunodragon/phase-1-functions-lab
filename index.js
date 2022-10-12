57aa4ef31647b9a7cd78895accb3f9a165c5c239
Andrew <andrewonulak@gmail.com>
2022-10-12 14:57:15 -0600

    commit tree 741d6838f5846c589c72f3dfc0b36166dc331fba

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation){
+    if (pickUpLocation > 42){
+        return (pickUpLocation - 42)
+    } else {
+        return (42 - pickUpLocation)
+    }
+}
+function distanceFromHqInFeet(value){
+    return (distanceFromHqInBlocks(value) * 264)
+}
+function distanceTravelledInFeet(start, finish){
+    if (start > finish) {
+        return ((start - finish) * 264)
+    } else {
+        return ((finish - start) * 264)
+    }
+}
+function calculatesFarePrice(start, destination){
+    if (distanceTravelledInFeet(start, destination) < 400)
+        return 0
+    else if (distanceTravelledInFeet(start, destination) <= 2000)
+        return ((distanceTravelledInFeet(start, destination) - 400) * 0.02)
+    else if (distanceTravelledInFeet(start,destination) <= 2500)
+        return 25
+    else if (distanceTravelledInFeet(start, destination) > 2500)
+        return 'cannot travel that far'
+}
